1fcb9d949bee74db3a15b883cf1220a98d57036f
ZehraGun <zgun137@gmail.com>
2025-12-02 20:49:10 +0300

    Update Main.java

--- src/main/java/org/beginsecure/Main.java
+++ src/main/java/org/beginsecure/Main.java
@@ -1,32 +1,11 @@
 package org.beginsecure;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int width = 300;
-        int height = 300;
-
-        // 1. Create the blank image
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-        // 2. Get Graphics2D to draw on it
-        Graphics2D g = image.createGraphics();
-
-        // 3. Fill background with white (or any color)
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
-
-        g.dispose();
-
-        // 4. Save to file
-        ImageIO.write(image, "png", new File("blank.png"));
-
-        System.out.println("Created blank.png");
+        QRCodeMaker maker = new QRCodeMaker();
+        maker.placeFinder();
+        maker.createPNG();
     }
 }
